3bf9aa4567205863fccdea7389f930acf207a498
venky619 <67676267+venky619@users.noreply.github.com>
2020-10-25 10:35:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,6 +18,75 @@ from pyDes import *
 import logging
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
                           ConversationHandler)
+#!/usr/bin/python3
+import requests
+import sys
+import argparse
+import bs4
+import headers
+def arg():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--A", help=" To download song ", action="store_true")
+    parser.add_argument("-q", help=" available quality (96,144,240,320)  ",  action="store_true")
+    args, unknown = parser.parse_known_args()
+def sig():
+    try:
+        arg()
+        if sys.argv[1] == '-a' or '--A':
+            req = requests.get(sys.argv[2])
+            req_parse = bs4.BeautifulSoup(req.content.decode(), "html5lib")
+            content = req_parse.find_all("script")[40].prettify()
+            pi = []
+            for _ in content.split(','):
+                if "pid" in _:
+                    pi.append(_)
+            pid = eval((pi[0])[9:])['pid']
+            return pid   
+        else:
+            pass
+    except IndexError:
+        pass
+def song():
+    data = {
+        "__call":"song.getDetails",
+        "pids":sig(),
+        "api_version":"4",
+        "_format":"json",
+        "ctx":"web6dot0",
+        "_marker":"0"
+    }
+    req = requests.get("https://www.jiosaavn.com/api.php", headers=headers.headers, params=data)
+    pr = (req.json())
+    try:
+        arg()
+        if sys.argv[3]:
+            pix = int(sys.argv[4])
+            x1 = pr["songs"][0]["title"]
+            print("SONG : ", x1)
+            enc_url = pr["songs"][0]["more_info"]["encrypted_media_url"]
+            data = {
+                "__call":"song.generateAuthToken",
+                "url":enc_url,
+                "bitrate":pix,
+                "_format":"json",
+                "api_version":"4",
+                "_marker":"0",
+                "ctx":"web6dot0"
+            }
+            song_req = requests.post("https://www.jiosaavn.com/api.php", headers=headers.headers, data=data)
+            tokn_url = (song_req.json()["auth_url"])
+            dl = requests.get(tokn_url)
+            with open('{}.mp3'.format(x1), "wb") as jn:
+                jn.write(dl.content)
+        else:
+            pass            
+    except IndexError:
+        print("python3 saavn.py --h")
+    except KeyboardInterrupt:
+        print("Abort")
+if __name__ == "__main__":
+    song()
+
 ###############################################################
 welcome = "Hey {} I Am Saavn Downloader \n Please Send Me Saavn Link \n Report At  @aryanvikash "
 
